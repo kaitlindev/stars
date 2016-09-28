a9602ad3ae4fe6b46df7c5b2bead951bf31b5a87
Kaitlin Kidd <kaitlinkidd@mail.usf.edu>
2016-09-27 20:14:01 -0400

    Initial project.

---
+++ stars-app/server.php
@@ -0,0 +1,43 @@
+<?php	
+	
+	// Receive the Data from Client
+	$data = $_REQUEST;
+	$name = $data['star_name'];
+	
+	// Result Array
+	$result = array();
+	
+	// Data Array
+	$star_details = array( 
+		array('name' => 'Proxima Centauri', 'distance' => '4.243',  'constellation' => 'Centaurus',         'evolution' => 'Red Dwarf'),
+        array('name' => 'Alpha Centauri A', 'distance' => '4.37',   'constellation' => 'Centaurus',         'evolution' => 'Main Sequence'),
+        array('name' => 'Alpha Centauri B', 'distance' => '4.37',   'constellation' => 'Centaurus',         'evolution' => 'Main Sequence'),
+        array('name' => 'Barnards Star',    'distance' => '5.978',  'constellation' => 'Ophiuchus',         'evolution' => 'Red Dwarf'),
+        array('name' => 'Wolf 359',         'distance' => '7.86',   'constellation' => 'Leo',               'evolution' => 'Red Dwarf'),
+        array('name' => 'Lalande 21185',    'distance' => '8.31',   'constellation' => 'Ursa Major',        'evolution' => 'Red Dwarf'),
+        array('name' => 'Sirius A',         'distance' => '8.60',   'constellation' => 'Canis Major',       'evolution' => 'Main Sequence'),
+        array('name' => 'Sirius B',         'distance' => '8.60',   'constellation' => 'Canis Major',       'evolution' => 'White Dwarf'),
+        array('name' => 'Luyten 726-8 A',   'distance' => '8.73',   'constellation' => 'Cetus',             'evolution' => 'Red Dwarf'),
+        array('name' => 'Luyten 726-8 B',   'distance' => '8.73',   'constellation' => 'Cetus',             'evolution' => 'Red Dwarf'),
+        array('name' => 'Ross 154',         'distance' => '	9.60',  'constellation' => 'Sagittarius',       'evolution' => 'Red Dwarf'),
+        array('name' => 'Ross 248',         'distance' => '10.30',  'constellation' => 'Andromeda',         'evolution' => 'Red Dwarf'),
+        array('name' => 'Epsilon Eridani',  'distance' => '10.475', 'constellation' => 'Eridanus',          'evolution' => 'Main Sequence'),
+        array('name' => 'Lacaille 9352',    'distance' => '10.68',  'constellation' => 'Piscis Austrinus',  'evolution' => 'Red Dwarf'),
+        array('name' => 'Ross 128',         'distance' => '11.03',  'constellation' => 'Virgo',             'evolution' => 'Red Dwarf'),
+        array('name' => 'Luyten 789-6 A',   'distance' => '11.1',   'constellation' => 'Aquarius',          'evolution' => 'Red Dwarf'),
+        array('name' => 'Luyten 789-6 B',   'distance' => '11.1',   'constellation' => 'Aquarius',          'evolution' => 'Red Dwarf'),
+        array('name' => 'Luyten 789-6 C',   'distance' => '11.1',   'constellation' => 'Aquarius',          'evolution' => 'Red Dwarf'),
+        array('name' => 'Procyon A',        'distance' => '11.46',  'constellation' => 'Canis Minor',       'evolution' => 'Main Sequence'),
+        array('name' => 'Procyon B',        'distance' => '11.46',  'constellation' => 'Canis Minor',       'evolution' => 'White Dwarf')
+	);
+	
+	for( $count = 0 ; $count < count( $star_details ) ; $count++ ) {
+		if( stripos( $star_details[$count]['name'] , $name ) !== false ) {
+			array_push( $result , $star_details[$count] );
+		}
+	}
+	
+	// Return Response as JSON
+	echo json_encode( $result );
+	
+?>
